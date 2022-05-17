ea87e41394a347d6ae115fce5b709f0dab89fff2
Marygimm <marygimm.moreira@bringglobal.com>
2022-05-17 22:31:15 +0000

    update structs

--- InstagramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
+++ InstagramFirestoreTutorial/ViewModel/AuthenticationViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+struct LoginViewModel {
+    var email: String?
+    var password: String?
+    
+    var formIsValid: Bool {
+        guard let emailValue = email, let passwordValue = password else { return false }
+        return !emailValue.isEmpty && !passwordValue.isEmpty
+    }
+    
+    var buttonBackgroundColor: UIColor {
+        return formIsValid ? UIColor.blue : UIColor.systemPurple.withAlphaComponent(0.3)
+    }
+    
+    var buttonTitleColor: UIColor {
+        return formIsValid ? .white : UIColor(white: 1, alpha: 0.67)
+    }
+    
+}
+
+struct RegistrationViewModel {
+    var email: String?
+    var password: String?
+    var fullName: String?
+    var username: String?
+}
