45537ebc7c75c7ea25902b0e8c2e4b70eff131b9
Marygimm <marygimm.moreira@bringglobal.com>
2022-05-17 22:08:19 +0000

    create custom button to avoid duplicate code

--- InstagramFirestoreTutorial/View/CustomButton.swift
+++ InstagramFirestoreTutorial/View/CustomButton.swift
@@ -5,4 +5,21 @@
 //  Created by Mary Moreira on 17/05/2022.
 //
 
-import Foundation
+import UIKit
+
+
+class CustomButton: UIButton {
+    init(title: String) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        setTitleColor(.white, for: .normal)
+        backgroundColor = .blue
+        layer.cornerRadius = 5
+        setHeight(50)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
