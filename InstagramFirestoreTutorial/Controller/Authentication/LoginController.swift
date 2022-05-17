3de8a6397acbec9c234c36241e6efa3a57686637
Marygimm <marygimm.moreira@bringglobal.com>
2022-05-17 22:07:47 +0000

    create login controller

--- InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
+++ InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -5,4 +5,83 @@
 //  Created by Mary Moreira on 12/05/2022.
 //
 
-import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var iconImage: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "Instagram_logo_white"))
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let textField = CustomTextField(placeholder: "Email")
+        textField.keyboardType = .emailAddress
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = CustomTextField(placeholder: "Password", secureTextEntry: true)
+        return textField
+    }()
+    
+    private lazy var loginButton = CustomButton(title: "Log in")
+    
+    private lazy var forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Forgot your password? ", secondPart: "Get help signing in")
+        return button
+    }()
+    
+    private lazy var dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Don't have an account? ", secondPart: "Sing Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handleShowSignUp() {
+        let controller = RegistrationController()
+        navigationController?.pushViewController(controller, animated: true)
+        
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        navigationController?.navigationBar.barStyle = .black
+        configureGradientLayer()
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 80, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField,
+                                                       loginButton, forgotPasswordButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        view.addSubview(stackView)
+        stackView.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                         paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+        
+    }
+    
+}
